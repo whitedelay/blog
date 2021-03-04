85c378cc42d623ed61621fb20d51406484507413
baekjiyeon <whitedelay@gmail.com>
2021-03-05 04:55:56 +0900

    title대신 slug로 url만들기 추가

--- lib/get-canonical-page-id.ts
+++ lib/get-canonical-page-id.ts
@@ -1,10 +1,43 @@
 import { ExtendedRecordMap } from 'notion-types'
 import {
   parsePageId,
-  getCanonicalPageId as getCanonicalPageIdImpl
+  uuidToId,
+  normalizeTitle,
+  getBlockTitle
 } from 'notion-utils'
 
 import { inversePageUrlOverrides } from './config'
+
+const getCanonicalPageIdImpl = (
+  pageId: string,
+  recordMap: ExtendedRecordMap,
+  { uuid = true }: { uuid?: boolean } = {}
+): string | null => {
+  if (!pageId || !recordMap) return null
+
+  const id = uuidToId(pageId)
+  const block = recordMap.block[pageId]?.value
+  if (block) {
+    let slug
+
+    try {
+      // get slug property
+      slug = block.properties['~rd{'][0][0]
+    } catch (err) {
+      // get title
+      slug = normalizeTitle(getBlockTitle(block, recordMap))
+    }
+    if (slug) {
+      if (uuid) {
+        return `${slug}-${id}`
+      } else {
+        return slug
+      }
+    }
+  }
+
+  return id
+}
 
 export function getCanonicalPageId(
   pageId: string,
