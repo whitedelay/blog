4169ac42249870b2f1241654c65f9bdb031d3f41
baekjiyeon <whitedelay@gmail.com>
2021-03-08 16:06:52 +0900

    Add google search console verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="google-site-verification" content="efjMNdIz7NsOOkbzUAEMvhjwXi-K7u7dLCpyY4zE49M" />
           <link
             rel='icon'
             href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✏️</text></svg>'
