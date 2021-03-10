52c9ebee6d554e0676a7f677e5066fdcae090a57
baekjiyeon <whitedelay@gmail.com>
2021-03-11 00:15:55 +0900

    Add lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ import { GA_TRACKING_ID } from '../lib/gtag'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='ko'>
         <Head>
           <meta
             name='google-site-verification'
